36107b1c18429cbc3de931c5acd8f34248b69eb2
Braulio Sespede <vans_graf@hotmail.com>
2013-04-30 14:54:44 -0300

    home
    typeahead + datepicker

---
+++ src/js/typeahead.js
@@ -0,0 +1,5 @@
+$(document).ready(function() { 
+    var subjects = ['Buenos Aires', 'Bueco','Cordoba','Corbata'];   
+    $('#origin').typeahead({source: subjects, minLength: 3})
+    $('#destination').typeahead({source: subjects, minLength: 3})
+});
