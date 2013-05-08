e27078fe4f35d5c210c09e88de6c339c83f00491
Braulio Sespede <vans_graf@hotmail.com>
2013-05-08 07:48:58 -0300

    Revert "Revert "todo indentado, agregado de comentarios y cambio de formato de fecha""
    This reverts commit dce38127d193f2312dbbedaa4aab0a7c893f0c0e.

--- src/js/slider.js
+++ src/js/slider.js
@@ -1,22 +1,18 @@
-$(document).ready(function() { 
-$(".noUiSlider").noUiSlider({
-    range: [0, 1000]
-   ,start: [0, 1000]
-   ,step: 100
-   ,slide: function(){
-        var values = $(this).val();
-        if (values[0] == 0){
-            values[0]="Sin mínimo";
-        }
-        if (values[1] == 1000){
-            values[1]="Sin máximo";
-        }
-        $("#min").text(            
-            values[0]
-        );
-        $("#max").text(
-            values[1]
-        );
-   }
-});
+$(document).ready(function() {
+	$(".noUiSlider").noUiSlider({
+		range : [ 0, 1000 ],
+		start : [ 0, 1000 ],
+		step : 100,
+		slide : function() {
+			var values = $(this).val();
+			if (values[0] == 0) {
+				values[0] = "Sin mínimo";
+			}
+			if (values[1] == 1000) {
+				values[1] = "Sin máximo";
+			}
+			$("#min").text(values[0]);
+			$("#max").text(values[1]);
+		}
+	});
 });
--- src/js/typeahead.js
+++ src/js/typeahead.js
@@ -1,5 +1,11 @@
-$(document).ready(function() { 
-    var subjects = ['Buenos Aires', 'Bueco','Cordoba','Corbata'];   
-    $('#origin').typeahead({source: subjects, minLength: 3})
-    $('#destination').typeahead({source: subjects, minLength: 3})
+$(document).ready(function() {
+	var subjects = [ 'Buenos Aires', 'Bueco', 'Cordoba', 'Corbata' ];
+	$('#origin').typeahead({
+		source : subjects,
+		minLength : 3
+	})
+	$('#destination').typeahead({
+		source : subjects,
+		minLength : 3
+	})
 });
