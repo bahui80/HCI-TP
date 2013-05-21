98d78c6d850b245e91ce11ada809c7c8d43d2e8f
Braulio Sespede <vans_graf@hotmail.com>
2013-05-21 03:02:03 -0300

    ofertas fix + coments fix + carrusel fix

--- src/js/promotions.js
+++ src/js/promotions.js
@@ -1,12 +1,15 @@
+var from;
+var from_id;
+
 $(document).ready(function() {
 		$.ajax({
             url: "http://eiffel.itba.edu.ar/hci/service2/Geo.groovy?method=GetCities&page_size=40",
 			dataType: "jsonp",
-			jsonpCallback: "fillCities",
+			jsonpCallback: "fillCitiesArray",
         });
 }); 
 
-function fillCities(data){
+function fillCitiesArray(data){
 	var myCities = new Array();
 	var myCitiesId = new Array();
 	var mapState = false;
@@ -24,22 +27,23 @@ function fillCities(data){
 		minLength : 3
 	});
 	
-	//$("#search_offers").click(function() {
-
-		var origin = $("#city_offers").val();	
-
-		origin = Math.floor(Math.random()*(myCities.length-1));
-		$("#offer_name").text("Ofertas desde "+myCities[origin]);
-		var my_lat = data['cities'][origin]['longitude'];
-		var my_long = data['cities'][origin]['longitude'];	
-		var origin_id = myCitiesId[origin];
+	$("#search_offers").click(function() {
+		$("#map-container").empty();
+		$("#map-text").empty();
+		$("#map-text").append('<i class="icon-spinner icon-spin"></i> Cargando');
+		var origin = $("#city_offers").val();		
+		var origin_id = myCitiesId[myCities.indexOf(origin)];
+		from = origin;
+		from_id = origin_id;
+		var my_lat = data['cities'][myCities.indexOf(origin)]['longitude'];
+		var my_long = data['cities'][myCities.indexOf(origin)]['longitude'];
 
 		$.ajax({
             url: "http://eiffel.itba.edu.ar/hci/service2/Booking.groovy?method=GetFlightDeals&from="+origin_id,
 			dataType: "jsonp",
 			jsonpCallback: "offer_search",
         });
-	//});		
+	});		
 }
 
 function offer_search(data){	
@@ -50,8 +54,7 @@ function offer_search(data){
 	var mapOptions = {
 		zoom: 2,
 		center: new google.maps.LatLng(0, 0),
-		mapTypeId: google.maps.MapTypeId.ROADMAP,
-		disableDefaultUI: true,
+		mapTypeId: google.maps.MapTypeId.ROADMAP
 	};
 	var map = new google.maps.Map(document.getElementById("map"), mapOptions);
 	
@@ -64,40 +67,16 @@ function offer_search(data){
 		var countryId = data['deals'][i]['countryName'];
 		var price = data['deals'][i]['price'];
 		var moneda = data['currencyId'];
-		var content = '<p><b>'+cityName+':</b> U$D '+price+'</p><a id="search_offer_btn'+i+'" class="btn btn-inverse thin-font btn-block">Buscar vuelo</a>'
+		var content = '<p><b>'+cityName+':</b> U$D '+price+'</p><a id="search_offer_btn'+i+'" data-from="'+from+' data-from-id="'+from_id+' data-to="'+cityName+'" data-to-id="'+cityId+'" data-price="'+price+'" class="btn btn-inverse thin-font btn-block" onclick="createCookie(this)">Buscar vuelo</a>'
 		if(price >0 && price<200){
-			marker[i] = createMarker(new google.maps.LatLng(offer_lat, offer_long), content, map, 'http://imageshack.us/a/img716/715/verycheap.png');
+			marker[i] = createMarker(new google.maps.LatLng(offer_lat, offer_long), content, map, 'http://img194.imageshack.us/img194/715/verycheap.png');
 		} else if (price >= 200 && price < 400){
-			marker[i] = createMarker(new google.maps.LatLng(offer_lat, offer_long), content, map, 'http://imageshack.us/a/img832/6568/cheapt.png');
+			marker[i] = createMarker(new google.maps.LatLng(offer_lat, offer_long), content, map, 'http://img822.imageshack.us/img822/9140/cheapf.png');
 		} else if (price >=400 && price < 1000){
-			marker[i] = createMarker(new google.maps.LatLng(offer_lat, offer_long), content, map, 'http://imageshack.us/a/img812/5279/normalik.png');
+			marker[i] = createMarker(new google.maps.LatLng(offer_lat, offer_long), content, map, 'http://img138.imageshack.us/img138/918/normalwy.png');
 		} else if (price >= 1000){
-			marker[i] = createMarker(new google.maps.LatLng(offer_lat, offer_long), content, map, 'http://imageshack.us/a/img59/4734/expensivep.png');
+			marker[i] = createMarker(new google.maps.LatLng(offer_lat, offer_long), content, map, 'http://img706.imageshack.us/img706/9034/expensivew.png');
 		}
-
-		$("#search_offer_btn"+i).click(function (){
-
-			//tomo la informacion que guarde en el boton
-			var from = 	$("#search_offer_btn"+i).data('from');
-			var from_id = $("#search_offer_btn"+i).data('from-id');
-			var to = $("#search_offer_btn"+i).data('to');
-			var to_id = $("#search_offer_btn"+i).data('to-id');
-			var price = $("#search_offer_btn"+i).data('price');
-			var today = new Date();
-			var dep_date = new Date(today.getFullYear(), today.getMonth(), today.getDate()+7);
-			dep_date = dep_date.getFullYear()+"-"+dep_date.getMonth()+"-"+dep_date.getDate();
-
-			// ALGO NO FUNCIONA!!!
-			alert(to);
-			$.cookie('from', from, { path: '/' });
-			$.cookie('from_code', from_code, { path: '/' });
-			$.cookie('to', to, { path: '/' });
-			$.cookie('to_code', to_code, { path: '/' });
-			$.cookie('dep_date', dep_date, { path: '/' });
-
-			//voy a la pag de resultados
-			document.location.href="results.html";
-		});	
 	}
 }
 
@@ -120,4 +99,17 @@ function createMarker(point, content, map, img) {
 		infowindow.open(map, marker);
 	});
 	return marker;
+};
+
+function createCookie(button) {
+    //tomo la informacion que guarde en el boton
+    var from = $(button).data('from');
+    var from_id = $(button).data('from-id');
+    var to = $(button).data('to');
+    var to_id = $(button).data('to-id');
+    var price = $(button).data('price');
+    var today = new Date();
+    var dep_date = new Date(today.getFullYear(), today.getMonth(), today.getDate()+7);
+    dep_date = dep_date.getFullYear()+"-"+dep_date.getMonth()+"-"+dep_date.getDate();
+    console.log("Create Cookie");
 };
